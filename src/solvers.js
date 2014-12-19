9c1f13bafaa68db5c383b12cea18bac12a99f104
rmi22186 <rmi22186@gmail.com>
2014-12-18 22:58:44 -0800

    finish rooks. tweak Q

--- src/solvers.js
+++ src/solvers.js
@@ -19,7 +19,7 @@ window.findNRooksSolution = function(n) {
   var solution;
 
   var helperfunction = function(row) {
-    for (var col = 0; col < size; col ++) {                        //get the row which is an array of column indices
+    for (var col = 0; col < size; col++) {                        //get the row which is an array of column indices
       newBoard.togglePiece(row,col);                               // toggle piece
       if (newBoard.hasAnyRooksConflicts()) {                       // check if any Rook conflicts
         newBoard.togglePiece(row,col)                              // if conflict, retoggle piece, step through for loop
@@ -29,7 +29,8 @@ window.findNRooksSolution = function(n) {
           // console.log(board.rows());
           return newBoard;                                         // return board to upper "recursion level"
         } else {
-          return helperfunction(row++);                            // if no conflict, and row !== n-1, recurse with next row return board to
+          // console.log(row);
+          return helperfunction(row+1);                            // if no conflict, and row !== n-1, recurse with next row return board to
         }
       }
     }
@@ -37,7 +38,6 @@ window.findNRooksSolution = function(n) {
 
   solution = helperfunction(0);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  console.log(solution.rows());
   return solution.rows();
 }
 
@@ -54,20 +54,63 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = [];
-  //while column < n && row < n
-    //If no conflict place queen. If Q === n, push solution into solution array , else reset column to 0 and increment row by 1
-    //If conflict, increment column by one and recurse to line above.
+  var solution;
+  var newBoard = new Board({n:n});
+  var size = n;
 
-    //if row & column === n, get last queen from solution and move it to next column without conflict.
+  var helperfunction = function(row, col) {
+    newBoard.togglePiece(row,col);                               // toggle piece
 
-    //BASE CASES
-    //if after a queen is placed, queen === n, push solution
+    var remaining = n-(col + 1);
+    if(remaining > 0){
+      for(var j=(col+1); j<n; j++){
+        helperfunction(row, j);
+      }
+    }
 
+    if (newBoard.hasAnyRooksConflicts()) {                       // check if any Rook conflicts
+      newBoard.togglePiece(row,col)                              // if conflict, retoggle piece, step through for loop
+    } else {
+      if (row === n-1) {                                         // if we are at the last row
+        // board = newBoard;                                     // set board
+        // console.log(board.rows());
+        return newBoard;                                         // return board to upper "recursion level"
+      } else {
+        // console.log(row);
+        return helperfunction(row+1);                            // if no conflict, and row !== n-1, recurse with next row return board to
+      }
+    }
+  }
+  /*var helperfunction = function(row) {
+    var lastRow = n-1;
+    for (var col = 0; col < size; col++) {                          //get the row which is an array of column indices
+      newBoard.togglePiece(row,col);                                // toggle piece
+      if (!newBoard.hasAnyQueensConflicts()) {                       // check if any Rook conflicts
+        if (row === lastRow) {                                      // if we are at the last row
+          return newBoard;                                         // return board to upper "recursion level"
+        } else if {
 
+        }
+        else {
+          return helperfunction(row+1);                            // if no conflict, and row !== n-1, recurse with next row return board to
+        }
+      }
+      newBoard.togglePiece(row,col)                               // if conflict, retoggle piece, step through for loop
+    }
+  }
+*/
+  if ( n === 0) {
+    solution = new Board({n:n});
+  } else if ( n === 2) {
+    solution = new Board({n:n});
+  } else if ( n === 3) {
+    solution = new Board({n:n});
+  } else {
+    solution = helperfunction(0,0);
+  }
+  console.log(n);
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-
-  return solution;
+  return solution.rows();
 };
 
 
